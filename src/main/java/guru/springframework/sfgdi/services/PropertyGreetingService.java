ed83c82a3853961a83189b7d97f19a5e8af85d10
ATTOLODE <vincent.attolode@worldline.com>
2023-01-29 14:36:40 +0100

    using Spring qualifiers to specify concrete bean implementations

--- src/main/java/guru/springframework/sfgdi/services/PropertyGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/PropertyGreetingService.java
@@ -3,9 +3,9 @@ package guru.springframework.sfgdi.services;
 import org.springframework.stereotype.Service;
 
 @Service
-public class GreetingServiceImpl implements GreetingService{
+public class PropertyGreetingService implements GreetingService{
     @Override
     public String sayGreeting() {
-        return "Naka suba si ?";
+        return "Property : Naka suba si ?";
     }
 }
