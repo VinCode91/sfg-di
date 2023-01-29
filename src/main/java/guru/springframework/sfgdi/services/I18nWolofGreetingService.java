1ade05bd6766184dc0478dd77ef699d4f4ce358e
ATTOLODE <vincent.attolode@worldline.com>
2023-01-29 15:40:49 +0100

    use Spring default profile

--- src/main/java/guru/springframework/sfgdi/services/I18nWolofGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nWolofGreetingService.java
@@ -3,7 +3,7 @@ package guru.springframework.sfgdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("SN")
+@Profile({"SN", "default"})
 @Service("i18nService")
 public class I18nWolofGreetingService implements GreetingService{
     @Override
